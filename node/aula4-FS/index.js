5a886146810f3a4a1f872158f3be20acea2ecb72
matheuzin <mackingdead@gmail.com>
2024-01-14 00:31:10 -0300

    node

---
+++ node/aula4-FS/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises
+const path = require('path');
+
+//  o files sao os arquivos
+// fs.readdir(path.resolve(__dirname)) 
+//     .then(files => console.log(files))
+//     .catch(e => console.log(e))
+
+async function o() {
+    console.log('OLA MUNDO')
+}
+
+o()
+
+console.log('o')
